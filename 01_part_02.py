1feeac26817740023810c12c6fc1088a0f8c2375
rooklift <16438795+rooklift@users.noreply.github.com>
2023-12-01 15:01:41 +0000

    tighten up

--- 01_part_02.py
+++ 01_part_02.py
@@ -7,39 +7,30 @@ def nice_lines(filename):
 
 def first_value(s, words):
 
-	lowest_numeric_index = None
-	first_digit = None
+	lowest_index = None
+	ret = None
 
 	for n, c in enumerate(s):
 		if c.isdigit():
-			lowest_numeric_index = n
-			first_digit = int(c)
+			lowest_index = n
+			ret = int(c)
 			break
-
-	lowest_word_index = None
-	first_word = None
 
 	for word in words:
 		if word in s:
 			i = s.index(word)
-			if lowest_word_index == None or i < lowest_word_index:
-				lowest_word_index = i
-				first_word = word
+			if lowest_index == None or i < lowest_index:
+				lowest_index = i
+				ret = words.index(word)
 
-	if first_digit == None and first_word == None:
+	if ret == None:
 		raise(ValueError)
 
-	if first_word == None or lowest_numeric_index < lowest_word_index:
-		return first_digit
-
-	if first_digit == None or lowest_word_index < lowest_numeric_index:
-		return words.index(first_word)
-
-	raise(ValueError)
+	return ret
 
 def main():
 	count = 0
-	for line in nice_lines("input"):
+	for line in nice_lines("01_input.txt"):
 		first = first_value(line, words_normal)
 		last = first_value(line[::-1], words_reversed)
 		count += int("{}{}".format(first, last))
