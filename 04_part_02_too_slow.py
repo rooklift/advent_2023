c50edb2da657a6879fff475df3ab1af7b3889e45
rooklift <16438795+rooklift@users.noreply.github.com>
2023-12-04 11:28:15 +0000

    Update 04_part_02_too_slow.py

--- 04_part_02_too_slow.py
+++ 04_part_02_too_slow.py
@@ -5,35 +5,35 @@ def nice_lines(filename):
 class Card:
 	def __init__(self, line):
 		self.line = line
-	def num(self):
-		return int(self.line.split(":")[0].split()[1])
-	def count_creations(self):
+		self.num = int(self.line.split(":")[0].split()[1])
+		self.creations = None		# Done next...
+		self.make_creations()
+	def make_creations(self):
 		tokens1 = self.line.split(":")[1].split("|")[0].split()
 		tokens2 = self.line.split(":")[1].split("|")[1].split()
 		set1 = set(tokens1)
 		set2 = set(tokens2)
-		return len(set1.intersection(set2))
-	def scratch(self):
-		return range(self.num() + 1, self.num() + 1 + self.count_creations())
-	def copy(self):
-		return Card(self.line)		# Note: doesn't copy scratched status
+		self.creations = list(range(self.num + 1, self.num + 1 + len(set1.intersection(set2))))
 
 def main():
 
-	lines = nice_lines("04_example.txt")
+	lines = nice_lines("04_input.txt")
 
-	og_cards = [None]				# We won't actually touch these. We will start with copies.
+	og_cards = [None]				# We won't actually touch these. We will start with copies. The None exists to get correct indices.
+
 	for line in lines:
 		og_cards.append(Card(line))
 
 	active_cards = []
 
 	for card in og_cards[1:]:
-		active_cards.append(card.copy())
+		active_cards.append(card)
 
 	count = 0
 
 	while True:
+
+		print(count)
 
 		if len(active_cards) == 0:
 			break
@@ -43,13 +43,12 @@ def main():
 		new_cards = []
 
 		for card in active_cards:
-			new_card_nums = card.scratch()
-			for num in new_card_nums:
-				new_cards.append(og_cards[num].copy())
+			for num in card.creations:
+				new_cards.append(og_cards[num])
 
 		active_cards = new_cards
 
+	print("Answer:")
 	print(count)
-
 
 main()
