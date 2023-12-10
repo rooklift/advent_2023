163d7eb2676040a564247dd0b8ae7481fc516247
rooklift <16438795+rooklift@users.noreply.github.com>
2023-12-10 11:45:04 +0000

    Create 10_part_02.py

---
+++ 10_part_02.py
@@ -0,0 +1,8 @@
+# Strategy for Part 2:
+# Traverse the loop
+# Clear all non-loop spots to be dots, i.e. "."
+# Now traverse the loop again:
+#	If a dot is seen on the LEFT (from the POV of the traversal) flag it
+# Flood fill the flags through the dots
+# The flagged dots should now (I hope) be either all of the inside dots, or all of the outside dots
+
