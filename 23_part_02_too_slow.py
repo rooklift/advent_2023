615af63d0da6d6af2bc58cf8949dd075c0e237e0
rooklift <16438795+rooklift@users.noreply.github.com>
2024-01-09 15:13:02 +0000

    Create 23_part_02_too_slow.py

---
+++ 23_part_02_too_slow.py
@@ -0,0 +1,92 @@
+N = 1
+E = 2
+S = 3
+W = 4
+
+all_dirs = [N, E, S, W]
+
+vectors = {
+	N: [0, -1],
+	E: [1, 0],
+	S: [0, 1],
+	W: [-1, 0]
+}
+
+def make_2d_array(width, height):
+	ret = []
+	for x in range(width):
+		ret.append([])
+		for y in range(height):
+			ret[x].append(None)
+	return ret
+
+def parse(filename):
+	with open(filename) as f:
+		s = f.read()
+	s = s.replace("v", ".")
+	s = s.replace(">", ".")
+	lines = [line.strip() for line in s.split("\n") if line.strip() != ""]
+	width = len(lines[0])
+	height = len(lines)
+	world = make_2d_array(width, height)
+	for x in range(width):
+		for y in range(height):
+			world[x][y] = lines[y][x]
+	world[-2][-1] = "F"
+	return world
+
+def main():
+	world = parse("23_example.txt")
+	been = set()
+	been.add((1, 0))
+	print("Result:", longest(world, 1, 1, 1, been))		# Cheap hack - start after 1 step made, at [1,1]
+
+def longest(world, x, y, steps, been):
+
+	been = been.copy()
+
+	while True:
+
+		been.add((x, y))
+
+		valid_next_locs = []
+
+		for pnd in all_dirs:
+
+			vector = vectors[pnd]
+
+			px = x + vector[0]
+			py = y + vector[1]
+
+			if (px, py) in been:
+				continue
+
+			c = world[px][py]
+
+			if c == ".":
+				valid_next_locs.append((px, py))
+
+			if c == "F":
+				print(steps + 1)
+				return steps + 1
+
+		if len(valid_next_locs) == 0:
+			# print("Dead end: {} steps, x,y = {},{}".format(steps, x, y))
+			return None
+
+		elif len(valid_next_locs) == 1:
+			steps += 1
+			x = valid_next_locs[0][0]
+			y = valid_next_locs[0][1]
+			continue
+
+		else:
+			best = None
+			for loc in valid_next_locs:
+				dist = longest(world, loc[0], loc[1], steps + 1, been)
+				if dist != None and (best == None or dist > best):
+					best = dist
+			return best
+
+
+main()
